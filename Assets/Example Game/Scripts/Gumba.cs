b5a38a9cdf778f3d9437ba344a3286bf7d4b8296
COREY MITCHELL <L000245471@student.fife.ac.uk>
2018-02-26 12:51:23 +0000

    Added Enemies and Damage

---
+++ Assets/Example Game/Scripts/Gumba.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gumba : MonoBehaviour
+{
+    public float damage = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision detected");
+
+        Player mario = collision.collider.GetComponent<Player>();
+        mario.ApplyDamage(damage);
+    }
+}
--- Assets/Example Game/Scripts/Player.cs
+++ Assets/Example Game/Scripts/Player.cs
@@ -5,18 +5,23 @@ public class Player : MonoBehaviour {
 
     public string logtext = "Hello world again!";
     public float speed = 2;
+    public float health = 10;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
 
         Debug.Log(logtext);
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Debug.Log(logtext);
+    }
+
+    // Update is called once per frame
+    void Update() {
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.velocity = Vector2.right * speed;
     }
+
+    public void ApplyDamage(float damageToDeal)
+    {
+        health = health - damageToDeal;
+    }
 }
+// Nein
