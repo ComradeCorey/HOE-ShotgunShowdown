bbd64cf2ee4bfc1799016730f35876de8790c617
ComradeCorey <36156939+ComradeCorey@users.noreply.github.com>
2018-05-07 14:51:12 +0100

    Made the enemy move left and right

--- Assets/My Game/Scripts/enemyMovement.cs
+++ Assets/My Game/Scripts/enemyMovement.cs
@@ -3,14 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class enemyMovement : MonoBehaviour {
+	public float enemySpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+	public float distance;
+
+	private bool movingRight = true;
+
+	public Transform groundDetection;
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+		{
+		// Make the enemy move right
+		transform.Translate(Vector2.right * enemySpeed * Time.deltaTime);
+
+		// Set the Ray to detect when the character moves to an object
+		RaycastHit2D groundInfo = Physics2D.Raycast (groundDetection.position, Vector2.down, 2f);
+
+		// Changes direction on whether the character is close to falling off
+		if (groundInfo.collider == false)
+		{
+			if (movingRight == true) 
+			{
+				transform.eulerAngles = new Vector3 (0, -180, 0);
+				movingRight = false;
+			} else 
+			{
+				transform.eulerAngles = new Vector3 (0, 0, 0);
+				movingRight = true;
+			}
+		}
 	}
 }
